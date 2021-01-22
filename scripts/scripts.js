fe11dea8aceaf4f14d1007062698f097c42f7168
brunoamorim39 <brunoamorim39@yahoo.com>
2021-01-22 14:13:32 -0500

    Update 1 - still incomplete

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,10 +14,14 @@ function addBookToLibrary() {
 }
 
 const inputs = document.querySelector('library');
+const buttonFinalize = document.querySelector('button-finalize');
 
 // Button implementation for adding book to library
 const buttonAdd = document.querySelector('#new-book');
 buttonAdd.addEventListener('click', () => {
+    document.getElementById('new-book').disabled = true;
+    document.getElementById('remove-book').disabled = true;
+    
     let nameInputField = document.createElement('input');
     nameInputField.setAttribute('type', 'text');
     nameInputField.setAttribute('name', 'name');
@@ -44,7 +48,7 @@ buttonAdd.addEventListener('click', () => {
 
     let readYetInputLabel = document.createElement('label');
     readYetInputLabel.setAttribute('for', 'readYet');
-    readYetInputLabel.textContent = 'Read Yet?'
+    readYetInputLabel.textContent = 'Read Yet?';
     document.getElementById('library').appendChild(readYetInputLabel);
 
     let readYetInputField = document.createElement('input');
@@ -54,14 +58,41 @@ buttonAdd.addEventListener('click', () => {
     readYetInputField.setAttribute('class', 'readYet');
     document.getElementById('library').appendChild(readYetInputField);
 
+    let buttonConfirm = document.createElement('button');
+    buttonConfirm.setAttribute('id', 'confirm-book');
+    buttonConfirm.setAttribute('class', 'book-mod');
+    buttonConfirm.textContent = 'CONFIRM';
+    document.getElementById('button-finalize').appendChild(buttonConfirm);
+
+    let buttonCancel = document.createElement('button');
+    buttonCancel.setAttribute('id', 'cancel-book');
+    buttonCancel.setAttribute('class', 'book-mod');
+    buttonCancel.textContent = 'CANCEL';
+    document.getElementById('button-finalize').appendChild(buttonCancel);
+
     nameInputField.style.cssText = 'padding: 5px; margin: 20px 10px 5px 10px;';
     authorInputField.style.cssText = 'padding: 5px; margin: 20px 10px 5px 10px;';
     pagesInputField.style.cssText = 'padding: 5px; margin: 20px 10px 5px 10px;';
     readYetInputLabel.style.cssText = 'padding: 10px; margin: 20px 0 5px 10px;';
     readYetInputField.style.cssText = 'padding: 5px; margin: 20px 10px 5px 0;';
+    buttonConfirm = style.cssText = 'margin: 10px; padding: 10px;';
+    buttonCancel = style.cssText = 'margin: 10px; padding: 10px';
 });
 
+// Button implementation for removing book from library
 const buttonRemove = document.querySelector('#remove-book');
 buttonRemove.addEventListener('click', () => {
     
+});
+
+// Button implementation for confirming addition to library
+const buttonConfirm = document.querySelector('#confirm-book');
+buttonConfirm.addEventListener('click', () => {
+
+});
+
+// Button implementation for canceling addition to library
+const buttonCancel = document.querySelector('#cancel-book');
+buttonCancel.addEventListener('click', () => {
+
 });
